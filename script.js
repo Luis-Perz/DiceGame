29a591d87c7dd629c139773452f7d121471787de
Luis Perez <Luis998.ap@gmail.com>
2026-02-28 23:33:59 -0600

    added async functions for result and reset

--- script.js
+++ script.js
@@ -1,54 +1,37 @@
-let numStorage = []
-const diceImg = ['/img/dice-one.png', '/img/dice-two.png', '/img/dice-three.png',
-    '/img/dice-four.png', '/img/dice-five.png', '/img/dice-six.png',
-]
-document.getElementById('roll-btn').addEventListener('click', randomNumber)
+let numStorage = [];
+document.getElementById('roll-btn').addEventListener('click', rollDice)
 document.getElementById('reset-btn').addEventListener('click', gameReset);
-function getTotal(){
-    total = numStorage.reduce((sum, num) => sum + num, 0)
-    return total;
-}
-function winCheck(number){
-    if (number === 11){
-       displayResult("confetti")
-       document.getElementById('reset-btn').style.display = 'block';
+
+function winCheck(result){
+    document.getElementById('reset-btn').style.display = 'block';
+    if (result === "true"){
+       displayResult("confetti");
        document.getElementById('roll-btn').innerHTML = 'You Win!'
     }else{
-        console.log("You Won")
-        displayResult("fire")
-        document.getElementById('reset-btn').style.display = 'block'
+        displayResult("fire");
         document.getElementById('roll-btn').innerHTML = 'BOOM!';
     }
 }
 
-
-function displayResult(result){
-    const resultCont = document.getElementById('dice');
-    
-    const resultImg = document.createElement('img');
-    diceCont.innerHTML = '';
-
-    resultImg.src = `/img/${result}.png`;
-    resultImg.className = "icons-image";
-    resultImg.alt = `${result} image`;
-
-    resultCont.appendChild(diceImg);
+function displayIcon(number, result){
+    const iconCont = document.getElementById('dice');
+    const iconImg = document.createElement('img');
+    iconCont.innerHTML = '';
+    if(result === null){
+        iconImg.src = `img/dice-${number}.png`;
+        iconImg.className = "icons-image"
+        iconImg.alt = `dice-${number} image`;
+        iconCont.appendChild(iconImg);
+    }
+    else{
+        iconImg.src = `/img/${result}.png`;
+        iconImg.className = "icons-image";
+        iconImg.alt = `${result} image`;
+        iconCont.appendChild(iconImg);
+    }
 }
-function displayDice(number){
-   
-    const diceCont = document.getElementById('dice');
-    const diceImg = document.createElement('img');
-    diceCont.innerHTML = '';
 
-    diceImg.src = `img/dice-${number}.png`;
-    diceImg.className = "icons-image"
-    diceImg.alt = `dice-$(number} image`;
-
-    diceCont.appendChild(diceImg);
-    
-}
 function gameReset(){
-    numStorage = [];
     const diceCont = document.getElementById('dice');
     diceCont.innerHTML = '';
     document.getElementById('reset-btn').style.display = 'none';
@@ -57,28 +40,47 @@ function gameReset(){
 
 async function rollDice(){
     try{
-        // const response = await fetch("https://node-dice-roller.azurewebsites.net/roll");
-        const response = await fetch("http://127.0.0.1:5501/roll");
+        const response = await fetch("https://node-dice-roller.azurewebsites.net/roll");
         const data = await response.json();
-        console.log(data)
         if (data.msg !== ""){
-            console.log(data.msg);
+            getResult();
             return;
         }
         // API returns JSON value random number
         let number = data.number;
-        if (numStorage.length!== 3){
+
+        if (numStorage.length <= 3){
             numStorage.push(number);
             displayDice(number);
         }
-        if (numStorage.length === 3){
-            results = data.result;
-            winCheck(results);
-            
-        }
-     
     }
     catch (e){
         console.log("Error fetching dice rolls.")
+    }
+}
+
+async function getResult(){
+    try{
+        const response = await fetch("https://node-dice-roller.azurewebsites.net/result");
+        const data = await response.json();
+        const gameResult = data.result;
+        if (data.result !== ""){
+            winCheck(gameResult); 
+            resetGame();   
+        }
+    }
+    catch (e){
+        console.log("Error fetching game results.")
+    }
+}
+
+async function resetGame(){
+    try{
+        const response = await fetch("https://node-dice-roller.azurewebsites.net/reset");
+        console.log("reset game");
+        numStorage = [];
+    }
+    catch (e){
+        console.log("Error resetting game.")
     }
 }
